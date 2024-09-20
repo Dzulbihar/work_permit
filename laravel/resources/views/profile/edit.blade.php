b5fdf61d9cc29c5bfa7b4452714f6dab52befe0d
DZULBIHAR\dzul <ahmad.dzulbihar69@gmail.com>
2024-09-20 13:23:02 +0700

    halaman profil

--- laravel/resources/views/profile/edit.blade.php
+++ laravel/resources/views/profile/edit.blade.php
@@ -3,6 +3,26 @@
 @section('heading', 'Edit Profile')
 
 @section('content')
+
+<!-- Content Header (Page header) -->
+<section class="content-header">
+    <div class="container-fluid">
+        <div class="row mb-2">
+            <div class="col-sm-6">
+            </div>
+            
+            <div class="col-sm-6">
+                <ol class="breadcrumb float-sm-right">
+                    <li class="breadcrumb-item">
+                        <a href="{{ route('home') }}">Home</a>
+                    </li>
+                    <li class="breadcrumb-item active">Profile</li>
+                </ol>
+            </div>
+        </div>
+    </div>
+</section>
+
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
